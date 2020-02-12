ab855121d9a53206ef3ef02d83d70ab237447f84
NG <nathan.gervais@connect.auto>
2020-02-12 18:25:25 +0000

    Adds Swagger ui

---
+++ AccountCalculator/Commands/CreateEndOfDaysBalanceCalculationCommand.cs
@@ -0,0 +1,15 @@
+﻿using AccountCalculator.Dtos;
+using AccountCalculator.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace AccountCalculator.Commands
+{
+    public class CreateEndOfDaysBalanceCalculationCommand : IRequest<CalculatedBalancesResponse>
+    {
+        public string AccountId { get; set; }
+        public DateTime RequestDateTime { get; set; }
+        public List<TransactionDto> Transactions { get; set; }
+    }
+}
--- AccountCalculator/Startup.cs
+++ AccountCalculator/Startup.cs
@@ -1,21 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AccountCalculator.Options;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace AccountCalculator
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddMediatR(typeof(Startup));
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Account Calculator", Version= "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +41,19 @@ namespace AccountCalculator
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
 
             app.UseRouting();
 
