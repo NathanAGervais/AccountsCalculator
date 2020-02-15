0b4c7040431588a5d4793b8b61d78a8e116fd8c3
NG <nathan.gervais@connect.auto>
2020-02-15 15:35:11 +0000

    Adds transaction dto missing from previous commit

---
+++ AccountCalculator/Dtos/TransactionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountCalculator.Dtos
+{
+    public class TransactionDto
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public DateTime BookingDate { get; set; }
+        public string MerchantDetails { get; set; }
+        public string CreditDebitIndicator { get; set; }
+    }
+}
